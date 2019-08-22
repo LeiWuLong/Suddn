e713eb28ef212d9c8f02d206842ee5a6d016ad43
Roxas <83628@global.ul.com>
2019-08-22 08:42:19 +0800

    Add project files.

---
+++ Suddn/Suddn/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Suddn.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
